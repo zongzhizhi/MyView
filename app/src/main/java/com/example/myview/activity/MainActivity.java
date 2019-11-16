bb89a2f4b7e591ae5da179426201f1d4dcd2723f
zong <1546227652@qq.com>
2019-11-16 17:05:08 +0800

    test1 创建Activity包

--- app/src/main/java/com/example/myview/activity/MainActivity.java
+++ app/src/main/java/com/example/myview/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.myview;
+package com.example.myview.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.myview.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
